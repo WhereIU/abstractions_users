420a13048dbbef0fdac67acca2be7084b991d27a
Saint <max161125@gmail.com>
2024-04-17 14:50:03 +0300

    refact

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,16 +1,15 @@
-import UserProfileManager from'../index.js'
+import UserProfileManager from '../index.js';
 
 const profileManager = new UserProfileManager();
 
-profileManager.addUser({ name: "Alice", email: "alice@example.com" });
-profileManager.addUser({ name: "Bob", email: "bob@example.com" });
-
+profileManager.addUser({ name: 'Alice', email: 'alice@example.com' });
+profileManager.addUser({ name: 'Bob', email: 'bob@example.com' });
 
 console.log(profileManager.getAllUsers(), 'all users added'); // Выводит информацию о Alice и Bob
 
-profileManager.updateUser(1, { name: "Alicia" }); // Обновляет имя Alice на Alicia
+profileManager.updateUser(1, { name: 'Alicia' }); // Обновляет имя Alice на Alicia
 profileManager.removeUser(2); // Удаляет Bob
 
 console.log(profileManager.getAllUsers(), 'all users after remove and update');
 
-console.log(profileManager.findUserByName("Ali"), 'findByName Ali'); // Находит Alicia
+console.log(profileManager.findUserByName('Ali'), 'findByName Ali'); // Находит Alicia
--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-//const randomNum = (rangeLen, minNumber = 1) => Math.floor(Math.random()
+// const randomNum = (rangeLen, minNumber = 1) => Math.floor(Math.random()
 //* rangeLen + minNumber);
 const getUserId = (user) => user.id;
 const getUserName = (user) => user.name;
-const getUnuquieId = (users) => {
-  //const savedId = users.map((user) => getUserId(user));
-  //let id = randomNum(10000);
-  //for (let i = 0; savedId.length > i; i += 1) {
-  //  if (savedId[i] === id) {
-      //id = randomNum(10000);
-  //    i = 0;
-  //  }
-  //}
-  return users.at(-1) ? getUserId(users.at(-1)) + 1 : 1;
-}
+const getUnuquieId = (users) => (users.at(-1) ? getUserId(users.at(-1)) + 1 : 1);
+// const savedId = users.map((user) => getUserId(user));
+// let id = randomNum(10000);
+// for (let i = 0; savedId.length > i; i += 1) {
+//  if (savedId[i] === id) {
+// id = randomNum(10000);
+//    i = 0;
+//  }
+// }
 
 class UserProfileManager {
   constructor() {
     this.users = [];
   }
-};
+}
 
 (function () {
   this.addUser = function (user) {
     const uniqueId = getUnuquieId(this.users);
-    this.users.push({id: uniqueId, ...user});
+    this.users.push({ id: uniqueId, ...user });
   };
 
   this.removeUser = function (id) {
@@ -31,9 +29,10 @@ class UserProfileManager {
   };
 
   this.updateUser = function (id, newInfo) {
-    for (let i = 0; this.users.length > i; i += 1) {
-      if (getUserId(this.users[i]) === id) {
-        this.users[i] = {...this.users[i], ...newInfo};
+    const { users } = this;
+    for (let i = 0; users.length > i; i += 1) {
+      if (getUserId(users[i]) === id) {
+        users[i] = { ...users[i], ...newInfo };
         break;
       }
     }
@@ -47,6 +46,5 @@ class UserProfileManager {
     return this.users;
   };
 }).call(UserProfileManager.prototype);
-
 
 export default UserProfileManager;
